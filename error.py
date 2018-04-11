4f7184296115e6690da9a82c037e5168f7adc0f5
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-04-11 12:08:26 -0400

    update gradient descent curves

--- error.py
+++ error.py
@@ -41,3 +41,21 @@ def plot_regression_and_rss(scatter_trace, regression_trace, rss_calc_trace):
     fig.append_trace(regression_trace, 1, 1)
     fig.append_trace(rss_calc_trace, 1, 2)
     plotly.offline.iplot(fig)
+
+def trace_rmse(x_values, y_values, regression_lines):
+    errors = root_mean_squared_errors(x_values, y_values, regression_lines)
+    x_values_bar = list(map(lambda error: 'm: ' + str(error[0]) + ' b: ' + str(error[1]),errors))
+    y_values_bar = list(map(lambda error: error[-1], errors))
+    return dict(
+        x=x_values_bar,
+        y=y_values_bar,
+        type='bar'
+    )
+    
+def regression_and_rmse(scatter_trace, regression_traces, rss_calc_trace):
+    fig = tools.make_subplots(rows=1, cols=2)
+    for reg_trace in regression_traces:
+        fig.append_trace(reg_trace, 1, 1)
+    fig.append_trace(scatter_trace, 1, 1)
+    fig.append_trace(rss_calc_trace, 1, 2)
+    iplot(fig)
